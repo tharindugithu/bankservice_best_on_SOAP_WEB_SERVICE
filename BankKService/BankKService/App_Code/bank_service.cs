0ee5f2629378cd7b33b70d9a056af9c5a6b154a5
D.L THARINDU MANDUSANKA <91211707+tharindugithu@users.noreply.github.com>
2022-12-25 23:39:14 +0530

    Add files via upload

---
+++ BankKService/BankKService/App_Code/bank_service.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for bank_service
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class bank_service : System.Web.Services.WebService
+{
+    
+    public bank_service()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+
+    [WebMethod]
+    public double DepositMoney(double accout_balance,double deposit_money)
+    {
+        //Console.WriteLine("return value is :" + (accout_balance + deposit_money));
+        return accout_balance + deposit_money; 
+    }
+
+    [WebMethod]
+    public double WithdrawMoney(double accout_balance, double withdraw_money)
+    {
+        //Console.WriteLine("return value is :" + (accout_balance - withdraw_money));
+        return accout_balance - withdraw_money;
+    }
+
+    [WebMethod]
+    public double TransferMoney(double accout_balance, double transfer_money)
+    {
+       // Console.WriteLine("return value is :"+(accout_balance - transfer_money));
+        return accout_balance - transfer_money;
+    }
+
+
+}
+
---
+++ BankKService/BankKService/bank_service.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/bank_service.cs" Class="bank_service" %>
